2cf73cb49c5c7d4fd0615a44900610a64652db41
enfantbeni <trayemarcel1990@outlook.com>
2019-04-17 16:02:36 +0000

    message

---
+++ src/main/java/com/belife/policemanager/controller/CompteController.java
@@ -0,0 +1,38 @@
+package com.belife.policemanager.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.belife.policemanager.model.dto.SourcePrelevementDto;
+
+@Controller
+public class CompteController {
+	
+	private String identifiantSession=null;
+    SourcePrelevementDto sourceDto=new  SourcePrelevementDto();
+	
+	@RequestMapping(value = {"/gestionEditionCompte" }, method = RequestMethod.GET)
+    public String gestionEditionCompte(Model model,  HttpSession session) { 		
+		String resultat=null;
+		try {
+			identifiantSession=session.getAttribute("identifiantSession").toString().trim();
+		}
+		catch(Exception e) {
+			resultat="pageErreur";
+			return resultat;
+		}
+		
+		model.addAttribute("cheminAccueil",  "Accueil >");
+		model.addAttribute("cheminEditionCompte",  "Edition Compte >");
+		model.addAttribute("gestionEditionCompte", "gestionEditionCompte");
+		model.addAttribute("identifiantSession", identifiantSession);
+		model.addAttribute("titre", " Edition Compte");
+		model.addAttribute("menuNavigation", "menuNavigation");
+        return "utilisateur/accueilUtilisateur";
+
+	}
+}
